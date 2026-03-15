059c22d87b8aa13b72c2d321d49e2af71037ab69
dipherent1 <binyammulat244@gmail.com>
2026-03-15 19:18:17 +0300

    feat: implement URL fetching and title extraction using goquery

--- main.go
+++ main.go
@@ -2,8 +2,56 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
-func main(){
-	fmt.Println("Hello, World!")
+type Result struct {
+	URL   string `json:"url"`
+	Title string `json:"title"`
+}
+
+func fetchURL(url string, wg *sync.WaitGroup, ch chan<- Result) {
+
+	defer wg.Done()
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Printf("Error fetching the resp, err: %s", err)
+	}
+
+	defer resp.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+
+	if err != nil {
+		log.Printf("Error fetching the doc; err: %s", err)
+	}
+
+	title := doc.Find("title").Text()
+
+	ch <- Result{URL: url, Title: title}
+
+}
+
+func main() {
+	urls := []string{"https://example.com", "https://example.org"}
+
+	var wg sync.WaitGroup
+
+	ch := make(chan Result, len(urls))
+
+	for _, url := range urls {
+		wg.Add(1)
+		go fetchURL(url, &wg, ch)
+	}
+
+	wg.Wait()
+	close(ch)
+
+	for result := range ch {
+		fmt.Printf("URL: %s\nTITLE: %s\n", result.URL, result.Title)
+	}
+
 }
